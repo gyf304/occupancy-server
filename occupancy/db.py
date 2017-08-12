bf3f76b83a4686f7cb0f6a39c2f9225015effa0d
Yifan Gu <gyf304@users.noreply.github.com>
2017-08-12 12:00:28 -0400

    adding a existance check for db

--- occupancy/db.py
+++ occupancy/db.py
@@ -6,4 +6,4 @@ from . import config, model
 engine = create_engine(config.DB_URI, echo=False)
 session_factory = sessionmaker()
 session_factory.configure(bind=engine)
-model.Base.metadata.create_all(bind=engine)
+model.Base.metadata.create_all(bind=engine, checkfirst=True)
